650c0ca2898fc5fc9de133c4598940bbc7b30ee2
Nachooosss <jmrg5364@gmail.com>
2023-07-15 10:29:50 -0400

    add build files

---
+++ build/precache-manifest.e001b7c6a4b0f373d66eeded4b2fa51d.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "e7916e4c634fe4cd1ae9",
+    "url": "/static/css/main.5c12aed2.chunk.css"
+  },
+  {
+    "revision": "e7916e4c634fe4cd1ae9",
+    "url": "/static/js/main.e7916e4c.chunk.js"
+  },
+  {
+    "revision": "ee2fa2fc286181834050",
+    "url": "/static/js/1.ee2fa2fc.chunk.js"
+  },
+  {
+    "revision": "a51b0f83e11008f712e9",
+    "url": "/static/css/2.6d1da68c.chunk.css"
+  },
+  {
+    "revision": "a51b0f83e11008f712e9",
+    "url": "/static/js/2.a51b0f83.chunk.js"
+  },
+  {
+    "revision": "1b2c1f4001cca68273bf",
+    "url": "/static/js/runtime~main.1b2c1f40.js"
+  },
+  {
+    "revision": "e2f0e0fe43eb364791fb0e7e4003417b",
+    "url": "/static/media/cover.e2f0e0fe.jpg"
+  },
+  {
+    "revision": "c01307e7f099ceddd5657a62940a84d5",
+    "url": "/static/media/ifLogo.c01307e7.svg"
+  },
+  {
+    "revision": "c492dd4276d10e4762ca4e582ad88623",
+    "url": "/index.html"
+  }
+];
