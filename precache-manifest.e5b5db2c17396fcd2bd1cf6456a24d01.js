9b551a13b21cdd39712354551814743b0967e421
Nachooosss <jmrg5364@gmail.com>
2023-08-17 12:49:23 -0400

    Updates

---
+++ precache-manifest.e5b5db2c17396fcd2bd1cf6456a24d01.js
@@ -0,0 +1,126 @@
+self.__precacheManifest = [
+  {
+    "revision": "a56dadc25da2e8a6c74b",
+    "url": "/jesusreyes/static/css/main.5693ac80.chunk.css"
+  },
+  {
+    "revision": "a56dadc25da2e8a6c74b",
+    "url": "/jesusreyes/static/js/main.a56dadc2.chunk.js"
+  },
+  {
+    "revision": "086b4935a08e44fc8a7d",
+    "url": "/jesusreyes/static/js/1.086b4935.chunk.js"
+  },
+  {
+    "revision": "54646f88e5a567c3ddd1",
+    "url": "/jesusreyes/static/css/2.1da2e0d4.chunk.css"
+  },
+  {
+    "revision": "54646f88e5a567c3ddd1",
+    "url": "/jesusreyes/static/js/2.54646f88.chunk.js"
+  },
+  {
+    "revision": "351e55c3f20705bc4cf8",
+    "url": "/jesusreyes/static/js/runtime~main.351e55c3.js"
+  },
+  {
+    "revision": "fcb596a4f4e6b09e46e5a2914d174a7e",
+    "url": "/jesusreyes/static/media/ifLogo.fcb596a4.svg"
+  },
+  {
+    "revision": "4e7789d54a9923171e938b7ed4bad8d0",
+    "url": "/jesusreyes/static/media/IA-Avatar.4e7789d5.png"
+  },
+  {
+    "revision": "41c21482d55d5034747c7679bdea9c64",
+    "url": "/jesusreyes/static/media/avatar.41c21482.png"
+  },
+  {
+    "revision": "5d0bf6118896468c6b209adb841c0a31",
+    "url": "/jesusreyes/static/media/ifLogoTwo.5d0bf611.svg"
+  },
+  {
+    "revision": "a616c0e5bb664c9c300cb76ab156176d",
+    "url": "/jesusreyes/static/media/Programminglanguages.a616c0e5.jpg"
+  },
+  {
+    "revision": "53ecf2bb86e46d2eb947faea156ce757",
+    "url": "/jesusreyes/static/media/Frameworks.53ecf2bb.jpg"
+  },
+  {
+    "revision": "f6ece1367b79d0581862176e91820af4",
+    "url": "/jesusreyes/static/media/debuggin.f6ece136.png"
+  },
+  {
+    "revision": "e216f33310e7372353c601e1eab49b7e",
+    "url": "/jesusreyes/static/media/seo.e216f333.png"
+  },
+  {
+    "revision": "32b73c7352eacbb71707f26c9d0adbcc",
+    "url": "/jesusreyes/static/media/javascript.32b73c73.jpg"
+  },
+  {
+    "revision": "778009762ec0ccced5fb84494364acee",
+    "url": "/jesusreyes/static/media/java.77800976.jpg"
+  },
+  {
+    "revision": "3b4355690481116d195dd17fc1e280ac",
+    "url": "/jesusreyes/static/media/php.3b435569.png"
+  },
+  {
+    "revision": "a76dc8705fb561f773bfd43e4a0e6d09",
+    "url": "/jesusreyes/static/media/python.a76dc870.jpg"
+  },
+  {
+    "revision": "2a4fda873f3a8b0fd43cb55aeed2b7e2",
+    "url": "/jesusreyes/static/media/django.2a4fda87.jpg"
+  },
+  {
+    "revision": "35cab181cea04c35a254a60b9c6a39c6",
+    "url": "/jesusreyes/static/media/unittest.35cab181.png"
+  },
+  {
+    "revision": "cdcbfde1a1a82d959102a1a83f7bf09f",
+    "url": "/jesusreyes/static/media/integration.cdcbfde1.jpg"
+  },
+  {
+    "revision": "f6e1c29814958814e9c354cc08790e77",
+    "url": "/jesusreyes/static/media/priorization.f6e1c298.jpg"
+  },
+  {
+    "revision": "22bf70a490ddfe7de564e611defaf02f",
+    "url": "/jesusreyes/static/media/projectmanagement.22bf70a4.png"
+  },
+  {
+    "revision": "592e0562d8e02a6a0f5a889f40e6cde2",
+    "url": "/jesusreyes/static/media/collaboration.592e0562.jpeg"
+  },
+  {
+    "revision": "0517921e24dc62a85fa4380d89113d4b",
+    "url": "/jesusreyes/static/media/leadership.0517921e.jpg"
+  },
+  {
+    "revision": "4708e22b995d115eb5b15da4c71166b1",
+    "url": "/jesusreyes/static/media/teamwork.4708e22b.jpg"
+  },
+  {
+    "revision": "8d5d36a12a9eecdf06f34f9da26e8c0f",
+    "url": "/jesusreyes/static/media/empathy.8d5d36a1.jpg"
+  },
+  {
+    "revision": "4de1923c6255f7b117629ee4cc2edd54",
+    "url": "/jesusreyes/static/media/logocubana.4de1923c.jpg"
+  },
+  {
+    "revision": "614f4cedc7dc9a0e98d334b5cfebc5c4",
+    "url": "/jesusreyes/static/media/mincex.614f4ced.jpg"
+  },
+  {
+    "revision": "84c46caaca6b8d6f84b26b2f3ad8e2c9",
+    "url": "/jesusreyes/static/media/CV Jesús Miguel Reyes García 2023.84c46caa.pdf"
+  },
+  {
+    "revision": "8f39da072bd884e4aa8e031fa5a41319",
+    "url": "/jesusreyes/index.html"
+  }
+];
